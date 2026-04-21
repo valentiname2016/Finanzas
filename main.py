9e75eeab0395179776bbf580c0e08020b74f93bd
KarenG2907 <karengaleano1138170@correo.itm.edu.co>
2026-04-20 20:41:42 -0500

    Actualización

---
+++ main.py
@@ -0,0 +1,20 @@
+from datetime import date, datetime
+from portafolio import Portafolio
+
+
+ACCIONES_EMPRESAS = {
+    "AAPL":  "Apple Inc.",
+    "AMZN":  "Amazon.com Inc.",
+    "BAC":   "Bank of America",
+    "GOOGL": "Alphabet (Google)",
+    "JNJ":   "Johnson & Johnson",
+    "JPM":   "JPMorgan Chase",
+    "META":  "Meta Platforms",
+    "MSFT":  "Microsoft Corp.",
+    "NVDA":  "NVIDIA Corp.",
+    "TSLA":  "Tesla Inc.",
+    "V":     "Visa Inc.",   
+    "WMT":   "Walmart Inc.",
+    "XOM":   "Exxon Mobil Corp."
+}
+
--- portafolio.py
+++ portafolio.py
@@ -1,12 +1,36 @@
 from historial import ListaEnlazada
-from activos import 
-from transacciones import 
+from acciones import Acciones
+#from transacciones import 
 from datetime import date
 
 class Portafolio:
 
     def __init__(self,capitalInicial:float):
         self.capitalDisponible = capitalInicial
-        self.acciones = dict[str, """Acciones"""] = {}
+        self.acciones = dict[str, Acciones] = {}
+        #self.cdt/bonos
+        self.historialTransacciones = ListaEnlazada()
+        self.historialValor: list[tuple] = []
+        self.diaActual = 0
+
+        print(f"Portafolio creado con capital inicial de: {self.capitalDisponible}")
+
+    
+    def comprar(self, ticker:str, cantidad:int, fechaCompra:date=None) -> bool:
         
+        if ticker not in self.acciones:
+            print(f"  [!] {ticker} no se encuentra registrado. "
+                  f"Agrégala con agregar_accion() primero.")
+            return False
+
+        accion = self.acciones[ticker]
+        fecha  = fechaCompra or date.today()
+
+        print(f"  Verificando precio de {ticker} para el {fecha}...")
+        precio = accion.obtener_precio_en_fecha(fecha)
+
+        if precio == 0:
+            print(f"  [!] No se pudo obtener el precio de {ticker} "
+                  f"para la fecha {fecha}.")
+            return False
         
