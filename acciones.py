fa7323c87d2e99a6cd914399b98febd71f3cf898
vale <valentiname2016@gmail.com>
2026-04-25 20:50:08 -0500

    Corrección codigo

--- acciones.py
+++ acciones.py
@@ -1,46 +1,29 @@
 import yfinance as yf
-
+from datetime import timedelta
+ 
 class Acciones:
     def __init__(self, ticker):
-        self.ticker=ticker
-        self.precioInicial=0.0
-        self.precioMinimo=0.0
-        self.precioMaximo=0.0
-        self.precioCierre=0.0
-        self.dividendo=0.0
-
-    def obtenerDatos(self):
-        data=yf.download(self.ticker, period="1d")
-        
+        self.ticker = ticker
+ 
+    def obtener_datos_dia(self, fecha):
+        data = yf.download(self.ticker, start=fecha, end=fecha + timedelta(days=1), actions=True, progress=False)
+ 
         if not data.empty:
-            self.precioInicial=float(data['Open'].iloc[-1])
-            self.precioMinimo=float(data['Low'].iloc[-1])
-            self.precioMaximo=float(data['High'].iloc[-1])
-            self.precioCierre=float(data['Close'].iloc[-1])
-
-            if 'Dividends' in data.columns:
-                self.dividendo=float(data['Dividends'].iloc[-1])
-            else:
-                self.dividendo=0.0
-        else:
-            print("No se pudieron obtener datos")
-
-    def rentabilidad(self):
-        if self.precioInicial != 0:
-            return (self.precioCierre-self.precioInicial)/self.precioInicial
-        else:
-            return 0
-        
-    def mostrarDatos(self):
-        print(f"Acción: {self.ticker}")
-        print(f"Precio inicial: {self.precioInicial}")
-        print(f"Precio mínimo: {self.precioMinimo}")
-        print(f"Precio máximo: {self.precioMaximo}")
-        print(f"Precio cierre: {self.precioCierre}")
-        print(f"Dividendo: {self.dividendo}")
-        print(f"Rentabilidad: {self.rentabilidad():.2%}")
-
-
-        
-
-        
+            try:
+                close = float(data['Close'].iloc[0].item())
+                low = float(data['Low'].iloc[0].item())
+                high = float(data['High'].iloc[0].item())
+ 
+                dividendo = 0.0
+                if 'Dividends' in data.columns:
+                    dividendo = float(data['Dividends'].iloc[0].item())
+ 
+                return {
+                    "close": close,
+                    "low": low,
+                    "high": high,
+                    "dividendo": dividendo
+                }
+            except Exception as e:
+                return None
+        return None
