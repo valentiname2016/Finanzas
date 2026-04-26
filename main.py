1cbb274b8ba3cc8c7510fe9ff681cc6bfa1a5850
vale <valentiname2016@gmail.com>
2026-04-26 15:12:44 -0500

    Opciones menu

--- main.py
+++ main.py
@@ -56,3 +56,30 @@ while continuar == True:
             mi_p.agregar_accion(accion_elegida)
         else:
             print("Este ticker no existe.")
+
+    elif opcion == "3":
+        if len(mi_p.posiciones) == 0:
+            print("\nNo has agregado acciones a tu portafolio.")
+            print("Por favor agrega acciones primero")
+        else:
+            print("\nACCIONES DISPONIBLES")
+            for ticker_agregado in mi_p.posiciones:
+                nombre_empresa = ACCIONES_EMPRESAS[ticker_agregado]
+                print(f"-> {ticker_agregado} ({nombre_empresa})")
+            
+            ticker_compra = input("\nEscriba el ticker que desea comprar: ").upper()
+            
+            if ticker_compra in mi_p.posiciones:
+                cantidad = int(input(f"Cantidad de acciones de {ticker_compra}: "))
+                fecha_str = input("Fecha de compra (AAAA-MM-DD): ")
+                fecha_compra = date.fromisoformat(fecha_str)
+                
+                mi_p.comprar(ticker_compra, cantidad, fecha_compra)
+            else:
+                print("Esa acción no está agregada en el portafolio.")
+ 
+    elif opcion == "4":
+        tasa_anual = float(input("Tasa efectiva anual (ejemplo: 0.12): "))
+        monto_cdt = float(input("Cuánto capital quieres invertir en el CDT: "))
+        mi_p.agregar_cdt(tasa_anual, 365, monto_cdt)
+        
